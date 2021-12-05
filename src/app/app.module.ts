1caccf718e1f96acdb0ed4a869272c17df5ab5e4
jenymireya90 <jenymireya.a@gmail.com>
2021-12-05 09:34:36 -0600

    3 files modified, 4 added, prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import {CharacterComponent} from './components/character/character.component';
+import {ApiService} from './services/api.service';
 
 
 @NgModule({
-  imports:      [ BrowserModule],
-  declarations: [ AppComponent],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, HttpClientModule],
+  declarations: [ AppComponent, CharacterComponent],
+  bootstrap:    [ AppComponent],
+  providers: [ApiService]
 })
 export class AppModule { }
